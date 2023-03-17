09f8f3085b0cc2eadf318b140022ea19af7f97f3
Ilya <ikalinin667@gmail.com>
2023-03-17 17:09:12 +0600

    123

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,13 @@
+import Header from "./components/Header"
+
 import style from "./App.module.css"
 
 export default function App() {
     return (
         <div className={style.container}>
-            <header>123</header>
+            <header>
+                <Header />
+            </header>
             <main>456</main>
             <nav>789</nav>
         </div>
