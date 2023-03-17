c8414a57f2b471019a12a3f1e65586b6f0ae1e43
Ilya <ikalinin667@gmail.com>
2023-03-17 17:20:10 +0600

    123

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,5 +1,10 @@
+import style from "./Header.module.css"
+
 export default function Header() {
     return (
-        <div></div>
+        <div className={style.container}>
+            <img src="https://visoff.ru/svg/CityHeroes_Icon.svg" alt="Лого CityHeroes" />
+            <button>Зарегистрироваться</button>
+        </div>
     )
 }
