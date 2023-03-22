0e802a79516237bb0124e73e0f955487e0614556
sashynkaDyrachek <ikalinin667@gmail.com>
2023-03-22 20:12:51 +0600

    123

--- src/frames/Home.tsx
+++ src/frames/Home.tsx
@@ -5,7 +5,7 @@ export default function Home() {
         <div className={style.container}>
             <h1>хакатон CityHeroes</h1>
             <h2 className="date">6 - 7 мая 2023</h2>
-            <img src="#" alt="Фото с последнего CityHeroes" />
+            <img src="https://visoff.ru/svg/prev_photo.jpg" alt="Фото с последнего CityHeroes" />
             <p>Описание хакатона, инфа и тд</p>
         </div>
     )
