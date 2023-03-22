f777e665786097cdd35b892ef1506ca293e43b09
sashynkaDyrachek <ikalinin667@gmail.com>
2023-03-22 17:40:19 +0600

    123

--- src/components/Main.tsx
+++ src/components/Main.tsx
@@ -3,7 +3,7 @@ import style from "./Main.module.css"
 export default function Main() {
     return (
         <div className={style.container}>
-            {new Array(10000).fill(1)}
+            {new Array(10000).fill(1).join("")}
         </div>
     )
 }
