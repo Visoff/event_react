99894b8f15274f196c60d184bce8bfa945030e52
sashynkaDyrachek <ikalinin667@gmail.com>
2023-03-22 20:47:17 +0600

    123

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,10 +1,13 @@
+import { useContext } from "react"
+import PopupContext from "../PopupContext"
 import style from "./Header.module.css"
 
 export default function Header() {
+    const [Active, setActive] = useContext(PopupContext)
     return (
         <div className={style.container}>
             <img src="https://visoff.ru/svg/CityHeroes_Icon.svg" alt="Лого CityHeroes" />
-            <button>Зарегистрироваться</button>
+            <button onClick={() => {setActive(true)}}>Зарегистрироваться</button>
         </div>
     )
 }
