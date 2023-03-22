e0e9b80f8e0277705a585618301702b33f1686b8
sashynkaDyrachek <ikalinin667@gmail.com>
2023-03-22 21:06:47 +0600

    123

--- src/components/Dialog.tsx
+++ src/components/Dialog.tsx
@@ -5,7 +5,7 @@ import style from "./Dialog.module.css"
 export default function Dialog() {
     const [Active, setActive] = useContext(PopupContext)
     return (
-        <div className={style.container + (Active ? " "+style.active : "")} onClick={(e) => {(e.target as HTMLElement).classList.contains(style.container) ? console.log(123) : console.log(456)}}>
+        <div className={style.container + (Active ? " "+style.active : "")} onClick={(e) => {(e.target as HTMLElement).classList.contains(style.container) ? setActive(false) : ""}}>
             <div className={style.main}>
                 123456
             </div>
