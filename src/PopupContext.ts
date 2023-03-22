37dfb616c3470d96b0b35c829664d75bca280538
sashynkaDyrachek <ikalinin667@gmail.com>
2023-03-22 21:08:21 +0600

    123

--- src/PopupContext.ts
+++ src/PopupContext.ts
@@ -1,5 +1,5 @@
 import { createContext, useState } from "react"
 
-const PopupContext = createContext()
+const PopupContext = createContext() as React.Context<[boolean, Function]>
 
 export default PopupContext
