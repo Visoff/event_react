98f035c9434f29c361a1f1bca016259f1c5a571e
sashynkaDyrachek <ikalinin667@gmail.com>
2023-03-22 19:54:37 +0600

    123

--- src/frames/Home.tsx
+++ src/frames/Home.tsx
@@ -1,7 +1,12 @@
+import style from "./Home.module.css"
+
 export default function Home() {
     return (
-        <div>
-            
+        <div className={style.container}>
+            <h1>хакатон CityHeroes</h1>
+            <h2 className="date">6 - 7 мая 2023</h2>
+            <img src="#" alt="Фото с последнего CityHeroes" />
+            <p>Описание хакатона, инфа и тд</p>
         </div>
     )
 }
