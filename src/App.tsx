d15c8ec48cd50f01883da29899a2c45340dbcea6
sashynkaDyrachek <ikalinin667@gmail.com>
2023-03-22 20:44:51 +0600

    123

--- src/App.tsx
+++ src/App.tsx
@@ -3,18 +3,23 @@ import Dialog from "./components/Dialog"
 import Home from "./frames/Home"
 
 import style from "./App.module.css"
+import { createContext, useContext, useState } from "react"
+import PopupContext from "./PopupContext"
 
 export default function App() {
+    const [PopupActive, setPopupActive] = useState(false)
     return (
         <div className={style.container}>
-            <header>
-                <Header />
-            </header>
-            <main>
-                <Home />
-            </main>
-            <nav>789</nav>
-            <Dialog />
+            <PopupContext.Provider value={[PopupActive, setPopupActive]}>
+                <header>
+                    <Header />
+                </header>
+                <main>
+                    <Home />
+                </main>
+                <nav>789</nav>
+                <Dialog />
+            </PopupContext.Provider>
         </div>
     )
 }
---
+++ src/PopupContext.ts
@@ -0,0 +1,5 @@
+import { createContext, useState } from "react"
+
+const PopupContext = createContext()
+
+export default PopupContext
