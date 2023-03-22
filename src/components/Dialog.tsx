6e61adcb2d21f6bd9d64d082940814a6e9c95249
sashynkaDyrachek <ikalinin667@gmail.com>
2023-03-22 21:00:57 +0600

    123

--- src/components/Dialog.tsx
+++ src/components/Dialog.tsx
@@ -4,7 +4,6 @@ import style from "./Dialog.module.css"
 
 export default function Dialog() {
     const [Active, setActive] = useContext(PopupContext)
-    const [st, setst] = useState(0)
     return (
         <div className={style.container + (Active ? " "+style.active : "")}>
             <div className={style.background}>123456</div>
