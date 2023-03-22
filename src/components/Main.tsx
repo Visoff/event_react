68c8ac0ae2464ef80c93068352cc920fb064893c
sashynkaDyrachek <ikalinin667@gmail.com>
2023-03-22 17:37:46 +0600

    123

--- src/components/Main.tsx
+++ src/components/Main.tsx
@@ -1,6 +1,8 @@
+import style from "./Main"
+
 export default function Main() {
     return (
-        <div>
+        <div className={style.container}>
             {new Array(10000).fill(1)}
         </div>
     )
