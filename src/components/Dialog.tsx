ad78531e00486246177b68a7432c21be2c0b28a0
sashynkaDyrachek <ikalinin667@gmail.com>
2023-03-22 20:50:49 +0600

    123

--- src/components/Dialog.tsx
+++ src/components/Dialog.tsx
@@ -1,9 +1,10 @@
-import { useContext, useState } from "react"
+import { useContext, useEffect, useState } from "react"
 import PopupContext from "../PopupContext"
 import style from "./Dialog.module.css"
 
 export default function Dialog() {
     const [Active, setActive] = useContext(PopupContext)
+    useEffect(() => {}, [Active])
     return (
         <div className={style.container + (Active ? " "+style.active : "")}>
             <div className={style.background}></div>
