3713cc6a781ac53501f97c07dd2013b9d29d2b0e
sashynkaDyrachek <ikalinin667@gmail.com>
2023-03-23 20:11:37 +0600

    123

--- src/components/Nav.tsx
+++ src/components/Nav.tsx
@@ -3,7 +3,9 @@ import style from "./Nav.module.css"
 export default function Nav() {
     return (
     <div className={style.container}>
-        123
+        <div>1</div>
+        <div>2</div>
+        <div>3</div>
     </div>
     )
 }
