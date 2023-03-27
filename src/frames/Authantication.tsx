ce3fc03c97cbf66f4fe027fe6a85f423e8509adc
Ilya <ikalinin667@gmail.com>
2023-03-27 10:16:09 +0600

    123

--- src/frames/Authantication.tsx
+++ src/frames/Authantication.tsx
@@ -6,10 +6,19 @@ export default function Auth() {
     const RegSubmit = (e:React.FormEvent<HTMLFormElement>) => {
         e.preventDefault()
     }
+    const [RegData, setRegData] = useState({
+        email:"",
+        name:"",
+        password:"",
+        repeat:""
+    })
 
     var registration = (
         <form onSubmit={RegSubmit}>
-
+            <input type="email" placeholder="email" onChange={(e) => {setRegData({...RegData, email:(e.target as HTMLInputElement).value})}} value={RegData.email} />
+            <input type="text" placeholder="name" onChange={(e) => {setRegData({...RegData, name:(e.target as HTMLInputElement).value})}} value={RegData.name} />
+            <input type="password" placeholder="password" onChange={(e) => {setRegData({...RegData, password:(e.target as HTMLInputElement).value})}} value={RegData.password} />
+            <input type="password" placeholder="repeat password" onChange={(e) => {setRegData({...RegData, repeat:(e.target as HTMLInputElement).value})}} value={RegData.repeat} />
         </form>
     )
 
