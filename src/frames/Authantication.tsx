b9d3b42324d10f069022aa1b525f31f65e93f77a
Ilya <ikalinin667@gmail.com>
2023-03-27 10:19:08 +0600

    123

--- src/frames/Authantication.tsx
+++ src/frames/Authantication.tsx
@@ -1,5 +1,7 @@
 import { useState } from "react"
 
+import style from "./Authantication.module.css"
+
 export default function Auth() {
     const [Mode, setMode] = useState("Reg")
 
@@ -14,11 +16,13 @@ export default function Auth() {
     })
 
     var registration = (
-        <form onSubmit={RegSubmit}>
+        <form className={style.form} onSubmit={RegSubmit}>
             <input type="email" placeholder="email" onChange={(e) => {setRegData({...RegData, email:(e.target as HTMLInputElement).value})}} value={RegData.email} />
             <input type="text" placeholder="name" onChange={(e) => {setRegData({...RegData, name:(e.target as HTMLInputElement).value})}} value={RegData.name} />
             <input type="password" placeholder="password" onChange={(e) => {setRegData({...RegData, password:(e.target as HTMLInputElement).value})}} value={RegData.password} />
             <input type="password" placeholder="repeat password" onChange={(e) => {setRegData({...RegData, repeat:(e.target as HTMLInputElement).value})}} value={RegData.repeat} />
+            <button>Зарегистрироваться</button>
+            <button className={style.switch} type="button" onClick={(e) => {setMode("login")}}>Уже есть акаунт?</button>
         </form>
     )
 
