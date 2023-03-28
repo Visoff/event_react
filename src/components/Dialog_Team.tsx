0c60b36b7a298a4b9adb9439e964213e686e4906
sashynkaDyrachek <ikalinin667@gmail.com>
2023-03-28 19:42:27 +0600

    123

--- src/components/Dialog_Team.tsx
+++ src/components/Dialog_Team.tsx
@@ -1,13 +1,15 @@
+import { MouseEventHandler } from "react"
 import style from "./Dialog.module.css"
 
 interface Props {
     name:string,
-    className?:string
+    className?:string,
+    onClick?:MouseEventHandler<HTMLButtonElement>
 }
 
-export default function Team({name, className}:Props) {
+export default function Team({name, className, onClick}:Props) {
     return (
-        <button className={style.team+(className != undefined ? " "+className : "")}>
+        <button onClick={(onClick != undefined ? onClick : (e) => {})} className={style.team+(className != undefined ? " "+className : "")}>
             <p className={style.name}>{name}</p>
         </button>
     )
