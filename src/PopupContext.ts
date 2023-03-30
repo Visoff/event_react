bd4468b356ccfb6e6141f0523c5fc149352db0e2
sashynkaDyrachek <ikalinin667@gmail.com>
2023-03-30 18:07:57 +0600

    123

--- src/PopupContext.ts
+++ src/PopupContext.ts
@@ -1,5 +1,5 @@
 import { createContext, Dispatch, SetStateAction, useState } from "react"
 
-const PopupContext = createContext(useState(false)) as React.Context<[boolean, Dispatch<SetStateAction<boolean>>]>
+const PopupContext = createContext([false, (() => {}) as Function]) as React.Context<[boolean, Function]>
 
 export default PopupContext
