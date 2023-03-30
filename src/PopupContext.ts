25da331f6c0701d51efc9644fd425803eb7cea20
sashynkaDyrachek <ikalinin667@gmail.com>
2023-03-30 19:03:14 +0600

    123

--- src/PopupContext.ts
+++ src/PopupContext.ts
@@ -1,4 +1,4 @@
-import { createContext, Dispatch, SetStateAction, useState } from "react"
+import { createContext } from "react"
 
 const PopupContext = createContext([false, (() => {}) as Function]) as React.Context<[boolean, Function]>
 
