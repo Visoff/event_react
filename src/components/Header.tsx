be7c6ff97a6e6014734294646e34aeeddc82859e
Ilya <ikalinin667@gmail.com>
2023-03-31 13:27:23 +0600

    123

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -6,7 +6,7 @@ export default function Header() {
     const [Active, setActive] = useContext(PopupContext)
     return (
         <div className={style.container}>
-            <button onClick={() => {console.log(123)}} style={{border:"none", background:"none", "height":"fit-content", "width":"fit-content"}}>
+            <button onClick={() => {console.log(123)}}>
                 <img src="https://visoff.ru/svg/CityHeroes_Icon.svg" alt="Лого CityHeroes" />
             </button>
             <button onClick={() => {setActive(true)}}>Зарегистрироваться</button>
