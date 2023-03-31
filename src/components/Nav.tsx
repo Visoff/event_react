de543dcb58d9c10bf2e27c31bafa431720669277
Ilya <ikalinin667@gmail.com>
2023-03-31 13:49:50 +0600

    123

--- src/components/Nav.tsx
+++ src/components/Nav.tsx
@@ -5,6 +5,7 @@ export default function Nav() {
     <div className={style.container}>
         <img src="https://visoff.ru/svg/home.svg" alt="home" />
         <img src="https://visoff.ru/svg/schedule.svg" alt="home" />
+        <img src="https://visoff.ru/svg/teams.svg" alt="home" />
         <img src="https://visoff.ru/svg/profile.svg" alt="home" />
     </div>
     )
